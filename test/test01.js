419d3ea20bf688a17759a483b452929937f9daa8
Aaron Castells <aaron@goxo.es>
2021-07-28 19:06:54 +0200

    solve problem

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,15 @@
 function tiers(index) {
-	return "";
+	let lines = []
+	const symbol = '*'
+	const lineLength = index
+	let result = ''
+	for(let i = 0; i < index; i++) {
+		let line = ''
+		line += ' '.repeat(lineLength - (i+1))
+		line += symbol.repeat(i+1)
+		line += '\n'
+		lines.push(line)
+	}
+	return lines.join('');
 }
 module.exports = tiers;
